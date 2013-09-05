1fb0850c0e1ee77dd161dcb57954a1a807153ca3
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-04 18:46:04 -0700

    whole file

--- public/js/application.js
+++ public/js/application.js
@@ -1,10 +1,17 @@
 $(document).ready(function () {
+  $('form').on('submit', function(e){
+    e.preventDefault();
 
-  // PSEUDO-CODE:
-  //   1- intercept the form submission event using jQuery
-  //   2- prevent the default action for that event from happening
-  //   3- generate a random number between 1 and 6 using JavaScript
-  //   4- use jQuery to submit an AJAX post to the form's action
-  //   5- when the AJAX post is done, replace the contents of the "#die" DIV in the DOM using jQuery
+    $.ajax({
+      type: this.method,
+      url: this.action,
+    }).done(function(response){
+      $('img').attr("src", response+".png");
+    }).fail(function(){
+      console.log("FAIL");
+    });
+  });
+
+
 
 });
